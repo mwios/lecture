8d37157bd572f683681d1ee90b88bf5e7679053b
Morgan Wilde <wilde.morgan@gmail.com>
2014-04-25 14:49:10 +0300

    header files

---
+++ Calculator.h
@@ -0,0 +1,13 @@
+//
+//  Calculator.h
+//  calculator
+//
+//  Created by Morgan Wilde on 25/04/2014.
+//  Copyright (c) 2014 morganwilde. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculator : NSObject
+
+@end
---
+++ ViewAction.h
@@ -0,0 +1,13 @@
+//
+//  ViewAction.h
+//  calculator
+//
+//  Created by Morgan Wilde on 25/04/2014.
+//  Copyright (c) 2014 morganwilde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewAction : UIButton
+
+@end
